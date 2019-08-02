0dbd76beab8987be024106e5f3852aae6fb3ae46
greykor <greykor@bk.ru>
2019-08-02 09:46:48 +0300

    server-app for 10 users

---
+++ serverBack.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+ 
+import socket
+ 
+serverBack = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverBack.bind(('0.0.0.0', 2222))
+serverBack.listen(10)
+client, addr = s.accept()
+while True:
+    data = client.recv(1024)
+    if len(data) > 1024 or data == 'close':
+        client.close()
+        break
+    else:
+        client.send(data)
+        print data
+    serverBack.close()
