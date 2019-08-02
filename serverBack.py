8608bb90936dc494da8a42709db7a6eda216ad0c
greykor <greykor@bk.ru>
2019-08-02 09:59:16 +0300

    server-app for 10 users | fixed logic

--- serverBack.py
+++ serverBack.py
@@ -5,13 +5,12 @@ import socket
 serverBack = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverBack.bind(('0.0.0.0', 2222))
 serverBack.listen(10)
-client, addr = serverBack.accept()
 while True:
-    data = client.recv(1024)
+	conn, addr = serverBack.accept()
+    data = conn.recv(1024)
     if len(data) > 1024 or data == 'close':
-        client.close()
         break
     else:
-        client.send(data)
+        conn.send(data)
         print data
     serverBack.close()
